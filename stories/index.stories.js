91534e1d9f2b040f906cada3a50460d7f1ac7be5
heineiuo <heineiuo@gmail.com>
2018-04-25 22:38:36 +0800

    Update package.json

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -4,10 +4,24 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
-import { Button, Welcome } from '@storybook/react/demo';
+import { View, Button, TextInput, Menu } from 'react-bucket';
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+storiesOf('Welcome', module).add('to Storybook', () => {
+  return (
+    <Welcome showApp={linkTo('Button')} />
+  )
+});
 
 storiesOf('Button', module)
   .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
   .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
+
+storiesOf('TextInput', module)
+  .add('with text', () => <TextInput />)
+
+storiesOf('Menu', module)
+  .add('basic', () => (
+    <View>
+      <Menu></Menu>
+    </View>
+  ))
