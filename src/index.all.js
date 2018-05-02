cc31aa019936e6f22de255b33e114d702f1b0e5b
heineiuo <heineiuo@gmail.com>
2018-05-02 23:16:49 +0800

    Update readme

---
+++ src/index.all.js
@@ -0,0 +1,12 @@
+import 'whatwg-fetch'
+
+export * from './Bucket'
+
+import * as Bucket from './Bucket'
+
+/**
+ * handle all packages
+ */
+import Webpack from './Webpack'
+
+export default Bucket
