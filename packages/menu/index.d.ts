bcbf6be58aea93c6562d00afe50ae37d50f47f2d
heineiuo <heineiuo@gmail.com>
2018-05-19 21:20:36 +0800

    Add index.d.ts

---
+++ packages/menu/index.d.ts
@@ -0,0 +1,7 @@
+// Type definitions for @react-web/button
+// Project: @react-web/button
+// Definitions by: heineiuo <https://github.com/heineiuo>
+
+import * as React from 'react';
+
+type P = any
